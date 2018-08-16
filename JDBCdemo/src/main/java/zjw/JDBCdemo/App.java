ec9e70055b597e6c6d54c689b3705ef0c332cda0
uidq1273 <uidq1273@HZH6433C.v01.net>
2018-08-16 10:29:13 +0800

    init

---
+++ JDBCdemo/src/main/java/zjw/JDBCdemo/App.java
@@ -0,0 +1,13 @@
+package zjw.JDBCdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Worlddd!" );
+    }
+}
