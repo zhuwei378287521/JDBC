b79fe59e019bbdc3ecf103fd1b35008654b86760
uidq1273 <uidq1273@HZH6433C.v01.net>
2018-08-16 13:30:57 +0800

    增加代码

---
+++ JDBCdemo/src/main/java/zjw/JDBCdemo/Test.java
@@ -0,0 +1,75 @@
+package zjw.JDBCdemo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+
+/**
+ * Hello world!
+ * 注意导入的包，有很多是java.sql的
+ * 查找username为zjw的数据。并全部打印出来。
+ */
+public class Test 
+{
+    public static void main( String[] args )
+    {
+    	Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		
+		try {
+			//加载数据库驱动
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			//通过驱动管理类获取数据库链接
+//			connection =  DriverManager.getConnection("jdbc:mysql://localhost:3306/mybatis?characterEncoding=utf-8", "root", "mysql");
+			connection =  DriverManager.getConnection("jdbc:mysql://192.168.237.132:3306/mybatis", "root", "123456");
+			//定义sql语句 ?表示占位符
+		String sql = "select * from user where username = ?";
+			//获取预处理statement
+			preparedStatement = connection.prepareStatement(sql);
+			//设置参数，第一个参数为sql语句中参数的序号（从1开始），第二个参数为设置的参数值
+			preparedStatement.setString(1, "zjw");
+			//向数据库发出sql执行查询，查询出结果集
+			resultSet =  preparedStatement.executeQuery();
+			//遍历查询结果集
+			while(resultSet.next()){
+				System.out.println("id="+resultSet.getString("id")+"  "+resultSet.getString("username"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally{
+			//释放资源
+			if(resultSet!=null){
+				try {
+					resultSet.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			if(preparedStatement!=null){
+				try {
+					preparedStatement.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			if(connection!=null){
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+		}
+
+    }
+}
